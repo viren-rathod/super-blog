f8d8724c632790853e611fb7cfac19d0fd877235
viren-rathod <viren.rathod.2023@gmail.com>
2023-05-24 15:01:18 +0530

    initial commit

---
+++ src/main/java/vrn/superblog/SuperBlogApplication.java
@@ -0,0 +1,13 @@
+package vrn.superblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vrn/superblog/SuperBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package vrn.superblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
